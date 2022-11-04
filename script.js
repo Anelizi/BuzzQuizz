ffe546a295eda1e8d067286c15d1fff2a3539b9a
expeditus303 <ricardotonietto@gmail.com>
2022-11-04 10:38:25 -0300

    renderizando todos os quizzes tela1 JS

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let quizzes = [];
 const todosQuizzes = document.querySelector('.todosQuizzes');
-const capaQuizz = document.querySelectorAll('.imagemCapaQuizz');
+
 
 
 pegarQuizzes()
@@ -30,19 +30,26 @@ function quizzesNaoRecebidos(quizzesNaoRecebidos) {
 function renderizarListaQuizzes() {
     todosQuizzes.innerHTML = ''
 
-    for (let i = 0; i < quizzes.length; i++) {
-
-        let styles = window.getComputedStyle(capaQuizz[i])
-
-        styles.backgroundImage = `url('https://uploads.jovemnerd.com.br/wp-content/uploads/2022/03/gandalf-colecionavel-01.jpg')`;
+    for (let i = 0; i + 2 < quizzes.length; i++) { // +2 aqui para deixar alinhado bonito no modo desktop, uma vez que recebemos 50 quizzes do servidor e 50 não é divisivel por 3;
 
         todosQuizzes.innerHTML += `
             <li class="quizz">
                     <div class="imagemCapaQuizz"></div>
-                    <p class="tituloQuizz">Acerte os personagens corretos dos Simpsons e prove seu amor!</p>
+                    <p class="tituloQuizz">${quizzes[i].title}</p>
                 </li>
         `
+
+        const capaQuizz = document.querySelectorAll('.imagemCapaQuizz');
+        console.log(capaQuizz)
+        let url = quizzes[i].image
+        capaQuizz[i].style.backgroundImage = `linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%), url(${url})`;
     }
 }
 
-/* capaQuizz[i].style.backgroundImage = `url('https://uploads.jovemnerd.com.br/wp-content/uploads/2022/03/gandalf-colecionavel-01.jpg')`; */
+// ---------------------------------------------------------------------------------
+
+
+// BOTÃO CRIAR QUIZZ TELA 1
+function criarQuizz() {
+    
+}
